0b560e561d8f53ce061b117696aa4b5014e6ff36
MathenaMaster <83555632+MathenaMaster@users.noreply.github.com>
2022-03-16 22:39:29 +0100

    Update checkContinuom.sh
    trolololol

--- SIGNAL_WORK/checkContinuom.sh
+++ SIGNAL_WORK/checkContinuom.sh
@@ -6,7 +6,7 @@ while [ $presentleft -gt 1 ];
     do sleep 2
     presentleft=$(ps -aux | grep './bibArt' | wc -l)
     presentleft=$(($presentleft - $min))
-    if [ $presentleft -lt 0 ]; then presentleft=0; fi
+    #if [ $presentleft -lt 0 ]; then presentleft=0; fi
     echo "$presentleft babies of biba alive!"
 done
 echo "No more"
